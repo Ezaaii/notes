b5084c0a3316ad6eafaeab637eda87671b6ad7c2
Unknown <n.vanroost@gmail.com>
2017-11-23 16:29:37 +0100

    test

--- index.php
+++ index.php
@@ -1,1 +1,26 @@
-hello je suis un test
+<?php
+try
+{
+    // On se connecte à MySQL
+    $pdo = new PDO('pgsql:host=ec2-46-137-174-67.eu-west-1.compute.amazonaws.com;dbname=df33soqaq9lked;', 'vwdpjdjljqoegd', 'd7912116db01deaa637ffdd67ce25adb5ad7b64323b444e9295c4faec5681cfd');
+}
+catch(Exception $e)
+{
+    // En cas d'erreur, on affiche un message et on arrête tout
+        die('Erreur : '.$e->getMessage());
+}
+$query = 'INSERT INTO categories (nom, description) VALUES (?, ?);';
+$prep = $pdo->prepare($query);
+
+$prep->bindValue(1, 'bertand', PDO::PARAM_STR);
+$prep->bindValue(2, 'ceci est un test pour desc', PDO::PARAM_STR);
+$prep->execute();
+$resultat = $pdo->query('SELECT * FROM categories');
+while ($donnees = $resultat->fetch())
+{
+  echo '<br/>';
+  echo $donnees['nom'];
+  echo ' : ';
+  echo $donnees['description'];
+}
+?>
